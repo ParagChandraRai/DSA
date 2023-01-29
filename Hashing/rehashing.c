09cf4922aee35b29efaa83f4671809193a373538
ParagChandraRai <80310600+ParagChandraRai@users.noreply.github.com>
2023-01-29 19:14:54 +0530

    Update rehashing.c

--- Hashing/rehashing.c
+++ Hashing/rehashing.c
@@ -1,94 +1,57 @@
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct node
+# define n 7
+int ht[n];
+void display()
 {
-	int data,key;
-	struct node* next;
-	
-}ps;
-ps * temp;
-int size,i=0,num,ch,j=0;
-void insert(ps ht[],int size)
-{
-	printf("Enter elemnet");
-	scanf("%d",&num);
-	temp=ht[num%size].next;
-	if(temp==0)
-	{
-		ps*nn=(ps*)malloc(sizeof(ps));
-		nn->data=num;
-		nn->next=0;
-		ht[num%size].next=nn;
-		
-	}
-
-else
-{
-	ps * nn=(ps*)malloc(sizeof(ps));
-	nn->data=num;
-		nn->next=0;
-		while(temp->next!=0)
-		{
-			temp=temp->next;
-			temp->next=nn;
-			temp=nn;
-		}
+    int i;
+    for(i = 0; i < n; i++)
+        printf("ht[%d] = %d\n",i,ht[i]);
 }
+void insert()
+{   
+    int value,i;
+    printf("Enter the value:");
+    scanf("%d",&value);
+    int key=key=(value%n +i)%n;//hash function 
+    if(ht[key]==-1)
+       ht[key]=value;
+    else
+    {
+    	for(i=0;i<n;i++)
+        {
+    	  key=(value%n + i+value%(n-2))%n;// different hash function 
+    	  printf("Collision Occurs:");
+    	  if(ht[key] == -1)
+          {     
+           ht[key] = value;
+           break;
+          }
+	    }
+	}
 }
-void search(ps ht[],int size)
-
-
-{
-	printf("Enter data");
-	scanf("%d",&num);
-	if(ht[num%size].next->data==num)
-	printf("Eneter at %d ",num%size);
-	else if(ht[num%size].next->next!=0)
-	{
-		ps * ptr =ht[num%size].next->next;
-		while(ptr->data!=num)
-		
-			ptr=ptr->next;
-			printf("Exists at %d ",num%size);
-			
-		
-	}
-	
-}
-void main()
-{
-	int i;
-	printf("Enetre size of table");
-	scanf("%d",&size);
-	ps ntable[size];
-	while(i<size)
-	{
-		ntable[i].next=0;
-		i++;
-	}
-	for(i=0;i<size;i++)
-	{
-		ntable[i].data=-1;
-		ntable[i].key=i;
-		
-	}
-	do
-	{
-		printf("choose");
-		scanf("%d",&ch);
-		switch(ch)
-		{
-		
-		case 1:
-			insert(ntable,size);
-			break;
-		case 2:
-			search(ntable,size);
-			break;
-		case 3:
-			display(ntable,size);
-			break;
-	}
-	}while(ch!=4);
-
+int main() {
+  int i, option;
+   
+  for (i = 0; i < n; i++)
+    ht[i] = -1;
+  while(1) 
+  {
+    printf("\n 1.Insert");
+    printf("\n 2.Display");
+    printf("\n 3.Exit");
+    printf("\n Enter your option.");
+    scanf("%d", & option);
+    switch (option) {
+    case 1:
+      insert();
+      break;
+    case 2:
+      display();
+      break;
+    case 3:
+    	exit(0);
+    }
+  }
+  return 0;
 }
